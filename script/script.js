fdcacd9245fe3af1256c3eac2621de9b777939d4
santjonatas <jonatascaina100@gmail.com>
2023-12-27 22:01:01 -0300

    em desenvolvimento curriculo

--- script/script.js
+++ script/script.js
@@ -2,18 +2,33 @@
 // funções informações
     // função formação
     function entrarMouseBotaoAbaFormacao(){
-        formacao.style.color = '#3637E5'
+        formacao.style.backgroundImage = 'linear-gradient(to left, #3637E5, #7037E6)'
+        formacao.style.display = 'inline-block'
+        formacao.style.webkitBackgroundClip = 'text'
+        formacao.style.color = 'transparent'
     }
     function sairMouseBotaoAbaFormacao(){
         formacao.style.color = 'white'
     }
     // função contact
     function entrarMouseBotaoAbaContact(){
-        contact.style.color = '#3637E5'
+        contact.style.backgroundImage = 'linear-gradient(to left, #3637E5, #7037E6)'
+        contact.style.display = 'inline-block'
+        contact.style.webkitBackgroundClip = 'text'
+        contact.style.color = 'transparent'
     }
     function sairMouseBotaoAbaContact(){
         contact.style.color = 'white'
     }
+    // função tema escuro
+    function entrarMouseBotaoLua(){
+        aba_tema.style.backgroundColor = '#333333'
+    }
+
+    function sairMouseBotaoLua(){
+        aba_tema.style.backgroundColor = ''
+    }
+
     // função github
     function entrarMouseBotaoAbaGithub(){
         github.style.boxShadow = '0px 0px 10px rgb(211, 211, 211)'
@@ -45,110 +60,50 @@
 // funções do menu principal
     // sobre mim
     function entrarMouseBotaoSobreMim(){
-        sobre_mim.style.backgroundImage = 'linear-gradient(to right, #0D1A37, #0D0D37, #1B0D37)'
+        sobre_mim.style.backgroundImage = 'linear-gradient(to right,  #376EE6, #3637E5, #7037E6)'
         sobre_mim.style.boxShadow = '0px 0px 15px #3637E5'
     }
     function sairMouseBotaoSobreMim(){
         sobre_mim.style.backgroundImage = 'linear-gradient(to right, #22448E, #22228E, #46228E)'
         sobre_mim.style.boxShadow = '0px 0px 5px #1C6572'
     }
+    function clicarMouseBotaoSobreMim(){
+        sobre_mim.style.backgroundImage = 'linear-gradient(to right, #0D1A37, #0D0D37, #1B0D37)'
+        aba_sobre_mim.scrollIntoView({ behavior: "smooth"})
+    }
+    function desclicarMouseBotaoSobreMim(){
+        sobre_mim.style.backgroundImage = 'linear-gradient(to right,  #376EE6, #3637E5, #7037E6)'
+    }
     // currículo
     function entrarMouseBotaoCurriculo(){
-        botao_curriculo.style.border = 'solid 3px #22448E'
-        botao_curriculo.style.color = '#22448E'
+        botao_curriculo.style.border = 'solid 3px #376EE6'
+        botao_curriculo.style.color = '#376EE6'
         botao_curriculo.style.boxShadow = '0px 0px 15px #3637E5'
     }
     function sairMouseBotaoCurriculo(){
+        botao_curriculo.style.border = 'solid 3px #22448E'
+        botao_curriculo.style.color = '#22448E'
+        botao_curriculo.style.boxShadow = '0px 0px 5px #1C6572'
+    }
+    function clicarMouseBotaoCV(){
+        botao_curriculo.style.border = 'solid 3px #0D1A37'
+        botao_curriculo.style.color = '#0D1A37'
+    }
+    function desclicarMouseBotaoCV(){
         botao_curriculo.style.border = 'solid 3px #376EE6'
         botao_curriculo.style.color = '#376EE6'
-        botao_curriculo.style.boxShadow = '0px 0px 5px #1C6572'
     }
     // foto de perfil
     function entrarMouseBotaoPerfil(){
-        gradient.style.boxShadow = '0px 0px 25px #3637E5' 
+        gradient.style.boxShadow = '0px 0px 27px #1C6572' 
     }
     function sairMouseBotaoPerfil(){
         gradient.style.boxShadow = '0px 0px 17px #1C6572'
     }
 
-// funções do botão da aba lateral
-function entrarMouseBotaoAbaLateral(){
-    aba_do_botao_da_aba_lateral.style.backgroundColor = '#012E40'
-    aba_do_botao_da_aba_lateral.style.borderRadius = '4px'
-    aba_do_botao_da_aba_lateral.style.boxShadow = '0px 0px 10px #24917d'
-}
-
-function sairMouseBotaoAbaLateral(){
-    aba_do_botao_da_aba_lateral.style.backgroundColor = '#01101d'
-    aba_do_botao_da_aba_lateral.style.borderRadius = '0px'
-    aba_do_botao_da_aba_lateral.style.boxShadow = 'none'
-}
-
-function clicarMouseBotaoAbaLateral(){
-    let aba_lateral = document.createElement('div')
-    aba_lateral.setAttribute('id', 'aba-lateral')
-    aba_lateral.setAttribute('class', 'scroll')
-
-    corpo.insertBefore(aba_lateral, cabecalho_e_menu)
-    aba_lateral.style.width = '300px'
-    aba_lateral.style.height = '100%'
-    aba_lateral.style.marginLeft = '-1049px'
-    aba_lateral.style.zIndex = '1'
-    aba_lateral.style.position = 'fixed'
-    
-    aba_do_botao_da_aba_lateral.removeChild(botao_da_aba_lateral)
-
-    let cabecalho_da_aba_lateral = document.createElement('div')
-    cabecalho_da_aba_lateral.setAttribute('id', 'cabecalho-da-aba-lateral')
-    aba_lateral.appendChild(cabecalho_da_aba_lateral)
-
-    cabecalho_da_aba_lateral.style.width = '300px'
-    cabecalho_da_aba_lateral.style.height = '61px'
-    cabecalho_da_aba_lateral.style.backgroundColor = 'red'
-
-    
-
-
-
-
-    
-
-    /*
-    function entrarMouseBotaoAbaLateralDeFechar(){
-        aba_botao_fechar_aba_lateral.style.backgroundColor = '#012E40'
-        aba_botao_fechar_aba_lateral.style.boxShadow = '0px 0px 10px #24917d'
-
-    }
-    function sairMouseBotaoAbaLateralDeFechar(){
-        aba_botao_fechar_aba_lateral.style.backgroundColor = '#025959'
-        aba_botao_fechar_aba_lateral.style.boxShadow = '0px 0px 5px #24917d'
-    }
-    function clicarMouseBotaoAbaLateralParaFechar(){
-        aba_botao_fechar_aba_lateral.style.backgroundColor = '#02735E'
-
-        corpo.removeChild(aba_lateral)
-        botao_e_portfolio.insertBefore(aba_do_botao_da_aba_lateral, portfolio)
-
-        cabecalho.style.justifyContent = 'space-around'
-        cabecalho.style.gap = '50%'
-        botao_e_portfolio.style.marginLeft = '0px'
-    }
-
-    botao_fechar_aba_lateral.addEventListener('mouseenter', entrarMouseBotaoAbaLateralDeFechar)
-    botao_fechar_aba_lateral.addEventListener('mouseout', sairMouseBotaoAbaLateralDeFechar)
-    botao_fechar_aba_lateral.addEventListener('click', clicarMouseBotaoAbaLateralParaFechar)
-    */
-}
-
-
-
 // ----- VARIÁVEIS -----
 // body
 let corpo = document.getElementById('corpo')
-// variáveis do botão da aba lateral
-let botao_da_aba_lateral = document.getElementById('botao-aba-lateral')
-    // div que contem o botao lateral
-    let aba_do_botao_da_aba_lateral =  document.getElementById('aba-botao-aba-lateral')
 
 // variável cabecalho e menu
 let cabecalho_e_menu = document.getElementById('cabecalho-e-menu')
@@ -163,6 +118,9 @@ let cabecalho_e_menu = document.getElementById('cabecalho-e-menu')
     let github =  document.getElementById('github')
     let linkedin = document.getElementById('linkedin')
     let instagram = document.getElementById('instagram')
+    // variável dos temas
+    let aba_tema = document.getElementById('aba-tema')
+    let botao_lua = document.getElementById('botao-lua')
 
 // variável do menu principal
 const menu_principal = document.querySelector('.menu')
@@ -170,16 +128,13 @@ const menu_principal = document.querySelector('.menu')
     // sobre mim
     let sobre_mim = document.getElementById('botao-sobre-mim')
     let texto_sobre_mim = document.getElementById('sobre-mim')
+    let aba_sobre_mim = document.getElementById('aba-sobre-mim')
     let botao_curriculo = document.getElementById('botao-curriculo')
     //foto de perfil
     let foto_perfil =  document.getElementById('foto-perfil')
     let gradient = document.getElementById('gradient')
 
 // ----- EVENTOS -----
-// eventos do botão da aba lateral
-botao_da_aba_lateral.addEventListener('mouseenter', entrarMouseBotaoAbaLateral)
-botao_da_aba_lateral.addEventListener('mouseout', sairMouseBotaoAbaLateral)
-botao_da_aba_lateral.addEventListener('click', clicarMouseBotaoAbaLateral)
 
 // eventos dos botões de navegação do header 'informações'
     // formação
@@ -198,13 +153,22 @@ botao_da_aba_lateral.addEventListener('click', clicarMouseBotaoAbaLateral)
     instagram.addEventListener('mouseenter', entrarMouseBotaoAbaInstagram)
     instagram.addEventListener('mouseout', sairMouseBotaoAbaInstagram)
 
+// eventos tema
+    // tema escuro
+    botao_lua.addEventListener('mouseenter', entrarMouseBotaoLua)
+    botao_lua.addEventListener('mouseout', sairMouseBotaoLua)
+
 // eventos menu principal
     // sobre mim
     sobre_mim.addEventListener('mouseenter', entrarMouseBotaoSobreMim)
     sobre_mim.addEventListener('mouseout', sairMouseBotaoSobreMim)
+    sobre_mim.addEventListener('mousedown', clicarMouseBotaoSobreMim)
+    sobre_mim.addEventListener('mouseup', desclicarMouseBotaoSobreMim)
     // currículo
     botao_curriculo.addEventListener('mouseenter', entrarMouseBotaoCurriculo)
     botao_curriculo.addEventListener('mouseout', sairMouseBotaoCurriculo)
+    botao_curriculo.addEventListener('mousedown', clicarMouseBotaoCV)
+    botao_curriculo.addEventListener('mouseup', desclicarMouseBotaoCV)
     // foto de perfil
     foto_perfil.addEventListener('mouseenter', entrarMouseBotaoPerfil)
-    foto_perfil.addEventListener('mouseout', sairMouseBotaoPerfil)
+    foto_perfil.addEventListener('mouseout', sairMouseBotaoPerfil) 
